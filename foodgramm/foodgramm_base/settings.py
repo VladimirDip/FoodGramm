7fff02d2748a36e3d41491baa05e6e7dbbaf6330
VladimirDip <kent4594111@gmail.com>
2022-07-28 13:52:57 +1000

    create new app recipes

--- foodgramm/foodgramm_base/settings.py
+++ foodgramm/foodgramm_base/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'recipes.apps.RecipesConfig',
 ]
 
 MIDDLEWARE = [
@@ -38,10 +40,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'foodgramm_base.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ foodgramm/recipes/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+from autoslug import AutoSlugField
+
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    title = models.CharField(
+        'Название ингридиента',
+        max_length=150,
+        db_index=True
+    )
+    dimension = models.CharField('Единица измерения', max_length=10)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'ингридиент'
+        verbose_name_plural = 'ингридиенты'
+
+    def __str__(self):
+        return f'{self.title}, {self.dimension}'
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор рецепта'
+    )
+    title = models.CharField('Название рецепта', max_length=200)
+    image = models.ImageField('Изображение', upload_to='recipes/')
+    text = models.TextField('Текст рецепта')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient'
+    )
+    cooking_time = models.PositiveIntegerField('Время приготовления')
+    slug = AutoSlugField(populate_from='title', allow_unicode=True)
+    tags = models.ManyToManyField('Tag', related_name='recipes')
+    pub_date = models.DateField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'рецепты'
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredients_amounts'
+    )
+    quantity = models.DecimalField(
+        max_digits=6,
+        decimal_places=1,
+        validators=[MinValueValidator(1)]
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=('ingredient', 'recipe'),
+                name='unique_recipe_ingredient'
+            )
+        ]
+
+
+class Tag(models.Model):
+    title = models.CharField(
+        'Имя тега',
+        max_length=50,
+        db_index=True
+    )
+    display_name = models.CharField('Имя тега для шаблона', max_length=50)
+    color = models.CharField('Цвет тега', max_length=50)
+
+    def __str__(self):
+        return self.title
